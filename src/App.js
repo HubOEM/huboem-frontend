e21b4aacc1178fdac15d74cef93a2a7048b9bf6c
Ethan Freeman <ethanfree23@gmail.com>
2022-06-13 21:12:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import HomePage from "./Pages/HomePage.js"
+
+import Navbar from "./Components/Navbar"
+
+
+function App() {
+  return (
+    <div>
+      {/* <div>
+        <Navbar />
+      </div> */}
+      <HomePage />
+    </div>
+  )
+}
+export default App
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import HeroSection from '../Components/HeroSection'
+import Dashboard from './Dashboard'
+import Settings from './Settings'
+import OrderSummary from './OrderSummary'
+import CheckoutForm from './CheckoutForm'
+import ShoppingCart from './ShoppingCart'
+import PartOverview from './PartOverview'
+import MachinesList from '../Components/MachinesList'
+import DocumentsList from './DocumentsList'
+import OrderFeed from '../Components/OrderFeed'
+
+function HomePage() {
+  return (
+    <div>
+        {/* <Dashboard /> */}
+        {/* <Settings /> */}
+        <HeroSection />
+        <OrderSummary />
+        <CheckoutForm />
+        <ShoppingCart />
+        <PartOverview />
+        <MachinesList />
+        <DocumentsList />
+        <OrderFeed />
+    </div>
+  )
+}
+
+export default HomePage
