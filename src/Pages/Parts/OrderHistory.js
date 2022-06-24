f684b199610d742e3c4e0b2acc7455929bed2269
Ethan Freeman <ethanfree23@gmail.com>
2022-06-24 16:18:08 -0500

    order history edit

--- src/Pages/Parts/OrderHistory.js
+++ src/Pages/Parts/OrderHistory.js
@@ -126,7 +126,7 @@ function OrderHistory() {
                         Order placed on <time dateTime={order.datetime}>{order.date}</time>
                       </h3>
 
-                      <div className="bg-gray-50 rounded-lg py-6 px-4 sm:px-6 sm:flex sm:items-center sm:justify-between sm:space-x-6 lg:space-x-8">
+                      <div className="bg-gray-50 border border-[#5ED797] rounded-lg py-6 px-4 sm:px-6 sm:flex sm:items-center sm:justify-between sm:space-x-6 lg:space-x-8">
                         <dl className="divide-y divide-gray-200 space-y-6 text-sm text-gray-600 flex-auto sm:divide-y-0 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-x-6 lg:w-1/2 lg:flex-none lg:gap-x-8">
                           <div className="flex justify-between sm:block">
                             <dt className="font-medium text-gray-900">Date placed</dt>
@@ -145,7 +145,7 @@ function OrderHistory() {
                         </dl>
                         <a
                           href={order.invoiceHref}
-                          className="w-full flex items-center justify-center bg-white mt-6 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:mt-0"
+                          className="shadow-apple w-full flex items-center justify-center bg-[#679de8b1] mt-6 py-2 px-4 border border-[#679CE8] rounded-md text-sm font-medium text-gray-700 hover:bg-[#679de8e0] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#679CE8] sm:w-auto sm:mt-0"
                         >
                           View Invoice
                           <span className="sr-only">for order {order.number}</span>
